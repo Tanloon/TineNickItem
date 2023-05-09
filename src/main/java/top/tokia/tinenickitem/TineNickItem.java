ddf63de47ecb6d39dddca759a50b7b1f1bfe5ff5
tantanjiang <tanloon@outlook.com>
2023-05-09 23:18:11 +0800

    Initial commit

---
+++ src/main/java/top/tokia/tinenickitem/TineNickItem.java
@@ -0,0 +1,17 @@
+package top.tokia.tinenickitem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TineNickItem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
